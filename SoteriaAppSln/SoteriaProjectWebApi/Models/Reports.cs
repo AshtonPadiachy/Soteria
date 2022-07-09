ada3dcb366fdaf77e9cff246919e297f37ca2002
SimamkeleSefalane <102727091+SimamkeleSefalane@users.noreply.github.com>
2022-07-09 13:30:22 +0200

    Added reports databases

---
+++ SoteriaAppSln/SoteriaProjectWebApi/Models/Reports.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoteriaProjectWebApi.Models
+{
+    public class Reports
+    {
+        [Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public string ReportsId { get; set; }
+
+        public Enum Zones { get; set; }
+
+        public string CommentOnZones { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        [ForeignKey("LearnerProfile")]
+        public string LearnerId { get; set; }
+
+        public int Participation { get; set; }
+
+        public string CommentOnParticipation { get; set; }
+        public int SocialSkills { get; set; }
+        public string CommentOnSocialSkills { get; set; }
+        public int CreativitySkills { get; set; }
+        public string CommentOnCreativitySkills { get; set; }
+        public string Tips { get; set; }
+
+        public ICollection<TeacherProfile>Teachers { get; set; }
+
+
+
+
+
+
+    }
+}
