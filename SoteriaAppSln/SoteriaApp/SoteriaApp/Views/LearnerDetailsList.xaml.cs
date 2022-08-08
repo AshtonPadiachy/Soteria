8f9d44658417514c4ba27434c337bfabe82cce34
SimamkeleSefalane <102727091+SimamkeleSefalane@users.noreply.github.com>
2022-08-08 09:22:21 +0200

    Added Learners Details List

---
+++ SoteriaAppSln/SoteriaApp/SoteriaApp/Views/LearnerDetailsList.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SoteriaApp.Views
+{
+    public partial class LearnerDetailsList : ContentPage
+    {
+        public LearnerDetailsList()
+        {
+            InitializeComponent();
+        }
+    }
+}
