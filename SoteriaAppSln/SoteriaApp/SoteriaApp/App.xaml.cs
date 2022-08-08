2a842dbe5c1753659d6870be5a66f58b93cb326d
SimamkeleSefalane <102727091+SimamkeleSefalane@users.noreply.github.com>
2022-08-08 11:08:32 +0200

    Added learner and parent input details on Learner details page

--- SoteriaAppSln/SoteriaApp/SoteriaApp/App.xaml.cs
+++ SoteriaAppSln/SoteriaApp/SoteriaApp/App.xaml.cs
@@ -95,7 +95,8 @@ namespace SoteriaApp
             containerRegistry.RegisterForNavigation<PupilReportPage, PupilReportPageViewModel>();
             containerRegistry.RegisterForNavigation<GradeClassListPage, GradeClassListPageViewModel>();
             containerRegistry.RegisterForNavigation<YellowZonesPage, YellowZonesPageViewModel>();
-            containerRegistry.RegisterForNavigation<DbdPage2, DbdPage2ViewModel>(); containerRegistry.RegisterForNavigation<LearnerReportsList, LearnerReportsListViewModel>();
+            containerRegistry.RegisterForNavigation<DbdPage2, DbdPage2ViewModel>(); 
+            containerRegistry.RegisterForNavigation<LearnerReportsList, LearnerReportsListViewModel>();
             containerRegistry.RegisterForNavigation<LearnerDetailsList, LearnerDetailsListViewModel>();
             containerRegistry.RegisterForNavigation<DepPage2, DepPage2ViewModel>();
         }
--- SoteriaAppSln/SoteriaApp/SoteriaApp/ViewModels/LearnerDetailsPageViewModel.cs
+++ SoteriaAppSln/SoteriaApp/SoteriaApp/ViewModels/LearnerDetailsPageViewModel.cs
@@ -1,16 +1,40 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace SoteriaApp.ViewModels
 {
-    public class LearnerDetailsPageViewModel : BindableBase
+    public class LearnerDetailsPageViewModel : ViewModelBase
     {
-        public LearnerDetailsPageViewModel()
+        //private DelegateCommand _learnerDetailsBtn;
+        //public DelegateCommand LearnerDetailsBtn =>
+         //   _learnerDetailsBtn ?? (_learnerDetailsBtn = new DelegateCommand(OnLearnerDetailsBtnCommand));
+        private void OnLearnerDetailsBtnCommand()
         {
-
+           // NavigationService.NavigateAsync("LearnerReportsList");
         }
+        public LearnerDetailsPageViewModel(INavigationService nav) : base(nav)
+        {
+        }
+        public string LearnerName { get; set; }
+        public string LearnerSurname { get; set; }
+        public string LearnerAge { get; set; }
+        public string LearnerGrade { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string IdNumber { get; set; }
+        public string ParentName { get; set; }
+        public string ParentSurname { get; set; }
+        public string Email { get; set; }
+        public string ParentAge { get; set; }
+        public string ParentGender { get; set; }
+        public string MaritalStatus { get; set; }
+        public string ParentCellNumber { get; set; }
     }
-}
+
+
+
+    }
+
