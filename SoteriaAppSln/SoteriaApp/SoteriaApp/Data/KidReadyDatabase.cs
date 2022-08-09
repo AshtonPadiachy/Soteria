159dff07fee76a09863a19c73ea03caeadf5636c
Atandiwe <102726464+Atandiwe@users.noreply.github.com>
2022-08-09 11:03:38 +0200

    commit added code

---
+++ SoteriaAppSln/SoteriaApp/SoteriaApp/Data/KidReadyDatabase.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SoteriaApp.Data
+{
+    public class KidReadyDatabase
+    {
+        private SQLiteConnection Database;
+        public  string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath,"KidReadyDatabase");
+            }
+        }
+        public KidReadyDatabase()
+        {
+            Database = new SQLiteConnection(DatabasePath, SQLite.SQLiteOpenFlags.ReadWrite |
+                // create the database if it doesn't exist
+                SQLite.SQLiteOpenFlags.Create |
+                // enable multi-threaded database access
+                SQLite.SQLiteOpenFlags.SharedCache
+);
+           // Database.CreateTable
+        }
+
+
+    }
+}
